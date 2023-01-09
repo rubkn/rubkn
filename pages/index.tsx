6e206a9e5ef4547288fef53e036523e564bd44b5
rubenbrandao <rubenavbrandao@gmail.com>
2023-01-09 17:24:32 +0000

    init with nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>rubkn</title>
+        <meta name="description" content="rubkn's personal website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>hello from the homepage</main>
+    </>
+  );
+};
+
+export default Home;
