fc53afdc00397c7e1b1e0aee8b31aabbaa935feb
rubenbrandao <rubenavbrandao@gmail.com>
2023-01-10 00:51:02 +0000

    feat: added pinned api

---
+++ pages/api/github.ts
@@ -0,0 +1,21 @@
+import { type NextRequest } from 'next/server';
+
+export const config = {
+  runtime: 'edge'
+};
+
+export default async function handler(req: NextRequest) {
+  const response = await fetch(`https://pinned.rubkn.dev/api/user/rubkn`, {
+    method: 'GET'
+  });
+
+  const data = await response.json();
+
+  return new Response(JSON.stringify({ data }), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json',
+      'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600'
+    }
+  });
+}
