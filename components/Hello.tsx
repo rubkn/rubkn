5f661db80c2749f407f5ad3f98fa5ac36daecd42
rubenbrandao <rubenavbrandao@gmail.com>
2023-02-08 19:16:33 +0000

    chore: add under construction text

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -11,6 +11,18 @@ const Hello = () => {
         inspiring people. Passionate about tech, cinema and analog photography.
         Love car rides and exploring new places.
       </div>
+      <div className="pt-4 text-base font-medium text-black-500 dark:text-black-100">
+        Please not that I'm still working on this website. You can check out the
+        staging version at{' '}
+        <a
+          href="https://staging.rubenbrandao.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black-500 hover:text-black-100 dark:text-black-100 dark:hover:text-black-200"
+        >
+          staging.rubkn.dev
+        </a>
+      </div>
     </main>
   );
 };
