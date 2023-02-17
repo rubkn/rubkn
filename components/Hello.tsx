78cd4b9150256c3d28d735f1bb20ff4d9d19b02e
rubenbrandao <rubenavbrandao@gmail.com>
2023-02-17 16:06:30 +0000

    chore: info experimental version

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -7,22 +7,22 @@ const Hello = () => {
         Ruben Brandão
       </div>
       <div className="pt-4 text-base font-medium text-black-500 dark:text-black-100">
-        A web developer looking to build creative projects and collaborate with
-        inspiring people. Passionate about tech, cinema and analog photography.
-        Love car rides and exploring new places.
-      </div>
-      <div className="pt-4 text-base font-medium text-black-500 dark:text-black-100">
-        Please note that I'm still working on this website. You can check out
-        the staging version at{' '}
+        Please note that this is a experimental version of my website. You can
+        find the live version at{' '}
         <a
-          href="https://staging.rubkn.dev"
+          href="https://rubkn.dev"
           target="_blank"
           rel="noopener noreferrer"
           className="text-black-500 hover:text-black-100 dark:text-black-100 dark:hover:text-black-200"
         >
-          staging.rubkn.dev
+          rubkn.dev
         </a>
       </div>
+      {/* <div className="pt-4 text-base font-medium text-black-500 dark:text-black-100">
+        A web developer looking to build creative projects and collaborate with
+        inspiring people. Passionate about tech, cinema and analog photography.
+        Love car rides and exploring new places.
+      </div> */}
     </main>
   );
 };
